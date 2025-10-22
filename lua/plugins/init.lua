e2d926d8836d44cb6268a20d173fb0f87538137c
Egor <barinov.egor2013@gmail.com>
2025-10-22 18:20:04 -0400

    upd: add multiple cursors plugin (vim-visual-multi)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -228,4 +228,17 @@ return {
       })
     end,
   },
+
+  -- Multiple cursors plugin
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    event = "VeryLazy",
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-m>",
+        ["Find Subword Under"] = "<C-m>",
+      }
+    end,
+  },
 }
